dd602acfc72db49deba3930d74e1bee35335b4d5
ericgarciaperez87-hub <eric.garcia.perez.87@gmail.com>
2026-04-16 19:47:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 
 // Sustituye las URLs por las de tu SharePoint (solo lectura)
 const VEHICLE_MODELS = {
-  "6F0": "./data/6F0.xlsx",
-  "5FP": "./data/5FP.xlsx",
-  "5FN": "./data/5FN.xlsx",
-  "5FF": "./data/5FF.xlsx",
-  "57S": "./data/57S.xlsx",
-  "10S": "./data/10S.xlsx",
-  "20H": "./data/20H.xlsx",
-  "6FA": "./data/6FA.xlsx",
-  "3FA": "./data/3FA.xlsx",
-  "2FA": "./data/2FA.xlsx",
-  "2FS": "./data/2FS.xlsx"
+  "6F0": "./data/6F0.csv",
+  "5FP": "./data/5FP.csv",
+  "5FN": "./data/5FN.csv",
+  "5FF": "./data/5FF.csv",
+  "57S": "./data/57S.csv",
+  "10S": "./data/10S.csv",
+  "20H": "./data/20H.csv",
+  "6FA": "./data/6FA.csv",
+  "3FA": "./data/3FA.csv",
+  "2FA": "./data/2FA.csv",
+  "2FS": "./data/2FS.csv"
 };
